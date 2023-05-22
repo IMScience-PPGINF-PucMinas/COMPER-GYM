f30a6d562cd272d31fac32d17583279da6bf1012
DanielENeves <danieleugenio.neves@gmail.com>
2023-05-22 05:12:25 +0000

    Results of trial4 for Ant-v4

--- COMPER_DDPG/comper_ddpg.py
+++ COMPER_DDPG/comper_ddpg.py
@@ -343,9 +343,9 @@ def grid_search():
     trainQTFreqquency=[1]    
     update_QTCritic_frequency=[1]
     q_lstm_bsize=[50000]    
-    trial=2
-    max_trial =2
-    log_base_dir ="logddpg" 
+    trial=4
+    max_trial =4
+    log_base_dir ="log" 
     config_trial_logger(base_log_dir = "./"+log_base_dir+"/"+task_name+"/trials/")
     agent=None
     while trial<=max_trial:
