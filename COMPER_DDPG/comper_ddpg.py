147e1b6228112a55841767ff65d18c70cca85923
DanielENeves <danieleugenio.neves@gmail.com>
2023-07-18 13:23:41 +0000

    COMPER-DDPG - Humanoid-v4 - Results from trial 1 using batch normalization, 0.0-0.001 for similarity and truncate signal

--- COMPER_DDPG/comper_ddpg.py
+++ COMPER_DDPG/comper_ddpg.py
@@ -336,15 +336,15 @@ def trial_log(log_data_dict):
         tl.dumpkvs()
 
 def grid_search():
-    task_name="HalfCheetah-v4"
+    task_name="Humanoid-v4"
     tota_iterations=[50000]
     lstm_epochs=[15]
     learningStartIter=[1]    
     trainQTFreqquency=[1]    
     update_QTCritic_frequency=[1]
     q_lstm_bsize=[50000]    
-    trial=2
-    max_trial =2
+    trial=1
+    max_trial =1
     log_base_dir ="log" 
     config_trial_logger(base_log_dir = "./"+log_base_dir+"/"+task_name+"/trials/")
     agent=None
