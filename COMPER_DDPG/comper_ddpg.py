092b396befdb19608c810ea3dae62f0273faca64
DanielENeves <danieleugenio.neves@gmail.com>
2023-10-04 14:47:46 +0000

    Hopper-v4 - DDPG - results from trial 6 to 10 using the signal of done and batch normalization

--- COMPER_DDPG/comper_ddpg.py
+++ COMPER_DDPG/comper_ddpg.py
@@ -336,7 +336,7 @@ def trial_log(log_data_dict):
         tl.dumpkvs()
 
 def grid_search():
-    task_name="HalfCheetah-v4"
+    task_name="Hopper-v4"
     tota_iterations=[50000]
     lstm_epochs=[15]
     learningStartIter=[100000]    
