112bce14996d71b3292927467e15c87c5d06808b
DanielENeves <danieleugenio.neves@gmail.com>
2023-10-04 03:43:38 +0000

    MountainCarContinuous-v0 - complets results from trial 6 to 10 using the signal of done and batch normalization

--- COMPER_DDPG/comper_ddpg.py
+++ COMPER_DDPG/comper_ddpg.py
@@ -336,7 +336,7 @@ def trial_log(log_data_dict):
         tl.dumpkvs()
 
 def grid_search():
-    task_name="InvertedPendulum-v4"
+    task_name="MountainCarContinuous-v0"
     tota_iterations=[50000]
     lstm_epochs=[15]
     learningStartIter=[10000000]    
